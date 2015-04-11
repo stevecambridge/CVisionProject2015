0a0107f7935c91769e65d193a1594a3362635c47
Thuy-Anh <thuy-anh@thuyanh-S400CA.(none)>
2015-04-11 14:54:04 -0400

    Added code

--- FaceRecognitionProject/main.cpp
+++ FaceRecognitionProject/main.cpp
@@ -12,8 +12,7 @@ struct Face_Bounding
     Point2d bottom_right;
     Mat image;
 };
-void Part1(vector<Face_Bounding> &faces);
-void Part2(vector<Mat> &Images);
+void Part1(images,histogramsForFaces,centers);
 Mat RANSACDLT(vector<Point2d> keypoints1, vector<Point2d> keypoints2);
 int* lbp_histogram(Mat img_window);
 int lbp_val(Mat img, int i, int j);
@@ -180,9 +179,200 @@ int main()
     //waitKey(0);
 
     // Call Part1 function
-    Part1(images);
-    // Call Part2 function
+    Mat histogramsForFaces, centers;
+    Part1(images,histogramsForFaces,centers);
+
+    /* Load the testing images */
+    vector<Mat> picturesTest;
+    // put the full address of the Training Images.txt here
+    const string trainingfilelistDamienTest = "/home/thuy-anh/CVisionProject2015/FaceRecognitionProject/damien.txt";
+    const string trainingfilelistSteveTest = "/home/thuy-anh/CVisionProject2015/FaceRecognitionProject/steveTest.txt";
+    const string trainingfilelistDanTest = "/home/thuy-anh/CVisionProject2015/FaceRecognitionProject/danTest.txt";
+    const string trainingfilelistThuyanhTest = "/home/thuy-anh/CVisionProject2015/FaceRecognitionProject/thuy-anhTest.txt";
+    // put the full address of the Training Images folder here
+    const string trainingBaseAddressDamienTest = "/home/thuy-anh/CVisionProject2015/FaceRecognitionProject/damienTest";
+    const string trainingBaseAddressDanTest = "/home/thuy-anh/CVisionProject2015/FaceRecognitionProject/danTest";
+    const string trainingBaseAddressSteveTest = "/home/thuy-anh/CVisionProject2015/FaceRecognitionProject/steveTest";
+    const string trainingBaseAddressThuyanhTest = "/home/thuy-anh/CVisionProject2015/FaceRecognitionProject/thuy-anhTest";
+    // Load the training dataset
+
+    YaleDatasetLoader(picturesTest, trainingBaseAddressSteveTest, trainingfilelistSteveTest);
+    YaleDatasetLoader(picturesTest, trainingBaseAddressDanTest, trainingfilelistDanTest);
+    YaleDatasetLoader(picturesTest, trainingBaseAddressThuyanhTest, trainingfilelistThuyanhTest);
+    //YaleDatasetLoader(picturesTest, trainingBaseAddressDamienTest, trainingfilelistDamienTest);
+
+    vector<Face_Bounding> imagesTest;
+    //Steve
+    Face_Bounding faceTest = {"steve",Point2d(122,106),Point2d(511,716),pictures[0]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(75,143),Point2d(462,661),pictures[1]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(138,132),Point2d(452,592),pictures[2]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(128,144),Point2d(436,604),pictures[3]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(6,68),Point2d(159,78),pictures[4]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(159,78),Point2d(569,591),pictures[5]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(71,81),Point2d(530,650),pictures[6]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(165,85),Point2d(518,580),pictures[7]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(111,74),Point2d(455,595),pictures[8]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(120,46),Point2d(443,515),pictures[9]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(100,39),Point2d(507,619),pictures[10]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(60,37),Point2d(426,527),pictures[11]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(111,56),Point2d(538,611),pictures[12]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"steve",Point2d(3,47),Point2d(406,548),pictures[13]};
+    imagesTest.push_back(faceTest);
+
+    //Dan pictures
+    faceTest = {"dan",Point2d(86,135),Point2d(492,622),pictures[14]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(118,106),Point2d(520,586),pictures[15]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(115,144),Point2d(434,566),pictures[16]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(108,158),Point2d(427,587),pictures[17]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(95,100),Point2d(509,671),pictures[18]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(102,142),Point2d(490,660),pictures[19]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(8,158),Point2d(477,779),pictures[20]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(30,120),Point2d(540,739),pictures[21]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(17,161),Point2d(512,757),pictures[22]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(44,98),Point2d(580,741),pictures[23]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(57,135),Point2d(520,660),pictures[24]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(105,156),Point2d(505,625),pictures[25]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(56,90),Point2d(555,637),pictures[26]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"dan",Point2d(156,124),Point2d(576,622),pictures[27]};
+    imagesTest.push_back(faceTest);
+
+    //Thuy-Anh
+    faceTest = {"thuy-anh",Point2d(119,93),Point2d(461,615),pictures[28]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh",Point2d(149,191),Point2d(480,631),pictures[29]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh",Point2d(117,194),Point2d(480,565),pictures[30]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(115,190),Point2d(455,643),pictures[31]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(120,100),Point2d(475,550),pictures[32]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(142,116),Point2d(454,574),pictures[33]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(119,132),Point2d(489,577),pictures[34]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(144,148),Point2d(477,554),pictures[35]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(96,138),Point2d(457,584),pictures[36]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(182,124),Point2d(500,527),pictures[37]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh",Point2d(107,114),Point2d(413,548),pictures[38]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh",Point2d(89,139),Point2d(406,548),pictures[39]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(111,120),Point2d(480,600),pictures[40]};
+    imagesTest.push_back(faceTest);
+    faceTest = {"thuy-anh", Point2d(66,118),Point2d(437,556),pictures[41]};
+    imagesTest.push_back(faceTest);
+
+    Mat histogramTestImages;
+    generateHistograms(imagesTest,centers,histogramTestImages);
+
     return 0;
+}
+
+void generateHistograms(vector<Face_Bounding> &faces, Mat centers, Mat histogramTestImages){
+
+    int s;
+    for (s = 0; s < faces.size(); s++){
+    Mat image1 = faces[s].image;
+    Ptr<FeatureDetector> FeatureDetectorSIFT = FeatureDetector::create("SIFT");
+    vector <KeyPoint> keyPoints1;
+
+    //Stores the keypoints for the first and second image
+    FeatureDetectorSIFT->detect(image1, keyPoints1);
+
+    //vector of face images with bounding boxes
+
+    Ptr<DescriptorExtractor> FeatureDescriptor = DescriptorExtractor::create("SIFT");
+
+    //Discard the keypoints that are outside of the bounding box
+    int i;
+    vector<int> keyPointsInBoxIndex;
+    vector<KeyPoint> keyPointsInBox;
+    //cv::rectangle(image1,faces[s].top_left,faces[s].bottom_right,1,8,0);
+    //point1.x =
+    for (i = 0; i < keyPoints1.size(); i++){
+        //if keypoint is inside then
+        bool inBox = in_box(keyPoints1[i].pt,faces[s].top_left,faces[s].bottom_right);
+        if (inBox){
+                keyPointsInBoxIndex.push_back(i);
+                keyPointsInBox.push_back(keyPoints1[i]);
+        }
+    }
+
+    Mat outputImageSIFT;
+    drawKeypoints(image1, keyPointsInBox, outputImageSIFT,Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    //imshow("Hello",outputImageSIFT);
+    //waitKey(0);
+
+    Mat extractedDescriptors1;
+    //stores the SIFT descriptors for the first and second image
+    FeatureDescriptor->compute(image1,keyPointsInBox,extractedDescriptors1);
+
+    double newNorm;
+    double norm = 10000;
+    int histogram[50];
+    int t;
+    for (t=0; t < 50; t++){
+        histogram[t]=0;
+    }
+    int center;
+    vector< vector<double> > centerOfDescriptors;
+    int x;
+    int i;
+    for(x = 0; x < extractedDescriptors1.rows; x++){
+        for (i = 0; i < centers.rows; i++){
+            //computes the euclidean distance between the two descriptors
+            newNorm = cv::norm(extractedDescriptors1.row(x) - centers.row(i));
+            //Store the best match with that descriptor
+            if (newNorm < norm){
+               norm = newNorm;
+               center = i;
+            }
+        }
+        //Pushes back the center to which the descriptor is matched. The centers
+        //are in the same order as the descriptors
+        centerOfDescriptors.push_back(centers.row(center));
+        histogram[center] = histogram[center] + 1;
+        norm = 10000;
+        center = 0;
+    }
+    int p;
+    for (p = 0; p < 50; p++){
+    cout << histogram[p] << "\n";
+    }
+    cout << "hello";
+    Mat histogramToMat = Mat(1,50,CV_8UC1,&histogram);
+    histogramTestImages.push_back(histogramToMat);
+    }
 }
 
 bool in_box(Point2d in, Point2d box1, Point2d box2)
@@ -250,10 +440,10 @@ bool YaleDatasetLoader(vector<Mat> &dataset, const string baseAddress, const str
     return true;
 }
 
-void Part1 (vector<Face_Bounding> &faces) {
+void Part1 (vector<Face_Bounding> &faces, Mat &histogramsForFaces, Mat &centers) {
     Mat descriptors;
     vector<Mat> descriptorsForEach;
-    Mat histogramsForFaces;
+
     int s;
     for (s = 0; s < faces.size(); s++){
     Mat image1 = faces[s].image;
@@ -292,8 +482,8 @@ void Part1 (vector<Face_Bounding> &faces) {
     FeatureDescriptor->compute(image1,keyPointsInBox,extractedDescriptors1);
     descriptors.push_back(extractedDescriptors1);
     descriptorsForEach.push_back(extractedDescriptors1);
-    }/*
-    Mat labels, centers;
+    }
+    Mat centers;
     kmeans(descriptors, 50, labels,
                 TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 10, 1.0),
                    3, KMEANS_PP_CENTERS, centers);
@@ -317,7 +507,7 @@ void Part1 (vector<Face_Bounding> &faces) {
     for(x = 0; x < descriptorsForEach[j].rows; x++){
         for (i = 0; i < centers.rows; i++){
             //computes the euclidean distance between the two descriptors
-            newNorm = cv::norm(descriptors.row(x) - centers.row(i));
+            newNorm = cv::norm(descriptorsForEach[j].row(x) - centers.row(i));
             //Store the best match with that descriptor
             if (newNorm < norm){
                norm = newNorm;
@@ -328,8 +518,6 @@ void Part1 (vector<Face_Bounding> &faces) {
         //are in the same order as the descriptors
         centerOfDescriptors.push_back(centers.row(center));
         histogram[center] = histogram[center] + 1;
-        //vector<int>::iterator iterator = histo.begin();
-        //histo.insert(iterato);
         norm = 10000;
         center = 0;
     }
@@ -340,7 +528,7 @@ void Part1 (vector<Face_Bounding> &faces) {
     cout << "hello";
     Mat histogramToMat = Mat(1,50,CV_8UC1,&histogram);
     histogramsForFaces.push_back(histogramToMat);
-    } */
+    }
 }
 
 void lbp_main(vector<Face_Bounding> faces)
